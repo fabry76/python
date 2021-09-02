de685a2657d0d2a5dab8327134bad7f92711b52e
fabry76 <fabrizio.fabiani@gmail.com>
2021-09-02 09:56:49 +0200

    new

--- 1.py
+++ 1.py
@@ -10,19 +10,26 @@ def clear_screen():
 
 def calculate(value):
     return f"\nThere are {value*60} minutes in {value} hours.\n"
-   
+      
 def check_int(value):
-    try:
-        var = int(value)
-        if var >= 0:
-            calc = calculate(var)
-            print(calc)
-    except:
-        print("\nYou have not entered an Integer number.\n")
+    if value == "exit" or value == "Exit":
+        exit()
+    else:
+        try:
+            var = int(value)
+            if var >= 0:
+                calc = calculate(var)
+                print(calc)
+        except:
+            print("\nYou have not entered an Integer number.\n")
 
+user_input = ""
 clear_screen()
-print("This program will return the number of minutes in (x) hours.\n")
-user_input = input("Enter the number of hours:\n(Multiple entries comma separated)\n")
-hours = user_input.split(",")
-for i in set(hours):
-    check_int(i)
+while user_input != "exit":
+    print("This program will return the number of minutes in (x) hours.\n")
+    user_input = input("Enter the number of hours:\n(Multiple entries comma separated)\n")
+    hours = user_input.split(",")
+    for i in set(hours):
+        check_int(i)
+        
+        
